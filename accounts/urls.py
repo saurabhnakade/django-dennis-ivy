edebee0e0ad5098bf3dbff7719b24253b06716e1
saurabhnakade <saurabh.nakade23@gmail.com>
2022-02-12 11:30:22 +0530

    dynamic url routing & create operation

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('products/',views.products),
-    path('customer/',views.customer),
+    path('', views.home,name="home"),
+    path('products/',views.products,name="products"),
+    path('customer/<str:key>/',views.customer,name="customer"),
+    path('create_order/',views.createOrder,name="create_order"), 
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
 from django.http import HttpResponse
 
 from .models import *
+from .forms import OrderForm
 
 def home(request):
     orders = Order.objects.all()
@@ -23,5 +24,24 @@ def products(request):
     products = Product.objects.all()
     return render(request,'accounts/products.html',{'products':products})
 
-def customer(request):
-    return render(request,'accounts/customer.html')
+def customer(request,key):
+    customer = Customer.objects.get(id=key)
+    orders=customer.order_set.all()
+    orders_count=orders.count()
+    context={'customer':customer,'orders':orders,'orders_count':orders_count}
+    return render(request,'accounts/customer.html',context)
+
+def createOrder(request):
+    form=OrderForm()
+
+    if request.method=='POST':
+        # print(request.POST)
+        form=OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context={
+        'form':form
+    }
+    return render(request,'accounts/order_form.html',context)
